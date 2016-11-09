969c716ad5003f32af1a6f6f02d1d2277e5b6927
Josiah Samuel <josiah_sams@in.ibm.com>
2016-11-09 18:45:33 +0530

    Update createDB.scala

--- tpcds_utils/createDB.scala
+++ tpcds_utils/createDB.scala
@@ -8,6 +8,9 @@ val DBNAME = "tpcds10tb"
 
 val KIT_PATH = "/home/baidu/tpcds-kit/tools"
 
+// For spark 2.0, uncomment the following line.
+// val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+
 val tables = new Tables(sqlContext, KIT_PATH, SIZE_IN_MB)
 
 sqlContext.sql(s"DROP DATABASE IF EXISTS $DBNAME CASCADE")
