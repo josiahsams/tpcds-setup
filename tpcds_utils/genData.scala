def99dea7d9d44c1e722df0fc334cfa323ca9087
Josiah Samuel <josiah_sams@in.ibm.com>
2016-11-09 18:44:36 +0530

    Update genData.scala

--- tpcds_utils/genData.scala
+++ tpcds_utils/genData.scala
@@ -6,6 +6,8 @@ val SIZE_IN_MB = 1024
 
 val KIT_PATH = "/home/baidu/tpcds-kit/tools"
 
+// For spark 2.0, uncomment the following line.
+// val sqlContext = new org.apache.spark.sql.SQLContext(sc)
 val tables = new Tables(sqlContext, KIT_PATH, SIZE_IN_MB)
 
 tables.genData(HDFS_PATH, "parquet", true, true, true, true, true)
