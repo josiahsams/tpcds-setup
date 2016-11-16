16003fad69cbf064f8d68528809d50ef6dda13f4
Josiah Samuel <josiah_sams@in.ibm.com>
2016-11-16 15:24:58 +0530

    Update genData.scala

--- tpcds_utils/genData.scala
+++ tpcds_utils/genData.scala
@@ -2,14 +2,14 @@ import com.databricks.spark.sql.perf.tpcds.Tables
 
 val HDFS_PATH = "hdfs://n001/TPCDS-1TB-test"
 
-val SIZE_IN_MB = 1024
+val SIZE_IN_GB = 1024
 
 val KIT_PATH = "/home/baidu/tpcds-kit/tools"
 
 // For spark 1.6, comment the following line.
 val sqlContext = new org.apache.spark.sql.SQLContext(sc)
 
-val tables = new Tables(sqlContext, KIT_PATH, SIZE_IN_MB)
+val tables = new Tables(sqlContext, KIT_PATH, SIZE_IN_GB)
 
 tables.genData(HDFS_PATH, "parquet", true, true, true, true, true)
 
