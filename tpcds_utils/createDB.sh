b3b1a7a999726ade6ce8b71eb2537922f1060bb0
Josiah Samuel <josiah_sams@in.ibm.com>
2016-11-16 15:26:22 +0530

    Update createDB.sh

--- tpcds_utils/createDB.sh
+++ tpcds_utils/createDB.sh
@@ -17,13 +17,13 @@ if [ $# -ne 3 ]; then
 fi
 
 hdfs_path=$1
-size_in_mb=$2
+size_in_gb=$2
 db_name=$3
 
 SCRIPT=${REPO_DIR}/tpcds_utils/createDB.scala
 SCRIPT_TO_EXECUTE=${REPO_DIR}/tpcds_utils/createDB.scala.$$
 
-sed "s~KIT_PATH =.*~KIT_PATH = \"${KIT_PATH}\"~g; s~DBNAME = .*~DBNAME = \"${db_name}\"~g ; s~HDFS_PATH =.*~HDFS_PATH = \"${hdfs_path}\"~g; s~SIZE_IN_MB =.*~SIZE_IN_MB = ${size_in_mb}~g" ${SCRIPT} > ${SCRIPT_TO_EXECUTE}
+sed "s~KIT_PATH =.*~KIT_PATH = \"${KIT_PATH}\"~g; s~DBNAME = .*~DBNAME = \"${db_name}\"~g ; s~HDFS_PATH =.*~HDFS_PATH = \"${hdfs_path}\"~g; s~SIZE_IN_GB =.*~SIZE_IN_GB = ${size_in_gb}~g" ${SCRIPT} > ${SCRIPT_TO_EXECUTE}
 
 # Below parameters will be initialized from run.config and can be overriden here
 # DRIVER_MEM=30g
